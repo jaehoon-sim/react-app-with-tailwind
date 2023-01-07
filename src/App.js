3d9aba00a0fc783153438c3277a9483a8162f5c7
JaehoonSim <18tomcat@gmail.com>
2023-01-07 14:12:06 +0900

    123

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Ridi from "./components/Ridi";
 import Series from "./components/Series";
 import Kakao from "./components/Kakao";
 import Joara from "./components/Joara";
+import Million from "./components/Million";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -19,8 +20,8 @@ function App() {
         <Series />
         <Kakao />
         <Joara />
-        {/* <Melon /> */}
-        <span>전체열기</span>
+        <Million />
+        {/* <span>전체열기</span> */}
       </div>
     </div>
   );
---
+++ src/components/Million.js
@@ -0,0 +1,53 @@
+import Nav from "./Nav.js";
+import NavItem from "./NavItem.js";
+import List from "../List.js";
+import MillionListItem from "./MillionListItem.js";
+import { useEffect, useState } from "react";
+import logo from "./million.PNG";
+
+function Million() {
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(true);
+  const ClickHandle = (event) => {
+    setShow(!show);
+  };
+  function DataFetch() {
+    useEffect(() => {
+      fetch(
+        "https://raw.githubusercontent.com/jaehoon-sim/kakaopageCrawler/master/million.json"
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((json_res) => {
+          setData(json_res);
+        });
+    }, []);
+  }
+  DataFetch();
+  return (
+    <div>
+      {show ? (
+        <div className="w-80 px-3 truncate lg:w-full">
+          <img src={logo} alt="logo" className="h-10 mt-6 mb-4" />
+          <span onClick={ClickHandle}>접기</span>
+          <div className="divide-y divide-slate-300 shadow-xl rounded-xl border h-screen scrollbar-thin scrollbar-none scrollbar-thumb-indigo-300 scrollbar-track-indigo-100 overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+            <Nav>
+              <NavItem href="#" isActive>
+                Daily
+              </NavItem>
+              <NavItem href="#">Weekly</NavItem>
+              <NavItem href="#">Monthly</NavItem>
+            </Nav>
+            <List>
+              {data.map((item) => (
+                <MillionListItem key={item.id} props={item} />
+              ))}
+            </List>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+export default Million;
